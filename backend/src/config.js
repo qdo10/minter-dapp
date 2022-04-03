50fd73bf6093d8c8f5200079135aabf65f7cac5a
Quang Do <d.lienquang@gmail.com>
2022-04-03 15:00:25 -0700

    commit

--- backend/src/config.js
+++ backend/src/config.js
@@ -7,7 +7,7 @@ const { NETWORK } = require(`${basePath}/constants/network.js`);
 const network = NETWORK.eth;
 
 // General metadata for Ethereum
-const namePrefix = "Urban Frenchie";
+const namePrefix = "Urban Frenchies";
 const description = "Urban Frenchies is a collection of 5,000 NFTs that come in a playful range of colors and traits. Each Urban Frenchie represents an ultramodern citizen of the web3 world who cares about the planet Earth.";
 const baseUri = "ipfs://NewUriToReplace"; // This will be replaced automatically
 
@@ -49,7 +49,7 @@ const LIMIT = 2; // Your API key rate limit
 const CHAIN = 'rinkeby'; // only rinkeby or polygon
 
 // REQUIRED CONTRACT DETAILS THAT CANNOT BE UPDATED LATER!
-const CONTRACT_NAME = 'Urban Frenchie';
+const CONTRACT_NAME = 'Urban Frenchies';
 const CONTRACT_SYMBOL = 'UFNFT';
 const METADATA_UPDATABLE = false; // set to false if you don't want to allow metadata updates after minting
 const OWNER_ADDRESS = '0xb1556F7Abb3e5D63cf7A8DA1c1cb804A38d51400';
