10ffe7e186ccfbdd867da9cee7965046404046d8
Quang Do <d.lienquang@gmail.com>
2022-04-03 17:17:48 -0700

    commit

--- backend/src/config.js
+++ backend/src/config.js
@@ -47,7 +47,7 @@ const extraMetadata = {
 // ** REQUIRED **
 const AUTH = process.env.NFTPORT_API_KEY; // Set this in the .env file to prevent exposing your API key when pushing to Github
 const LIMIT = 2; // Your API key rate limit
-const CHAIN = 'rinkeby'; // only rinkeby or polygon
+const CHAIN = 'polygon'; // only rinkeby or polygon
 
 // REQUIRED CONTRACT DETAILS THAT CANNOT BE UPDATED LATER!
 const CONTRACT_NAME = 'Urban Frenchies';
@@ -56,7 +56,7 @@ const METADATA_UPDATABLE = true; // set to false if you don't want to allow meta
 const OWNER_ADDRESS = '0xb1556F7Abb3e5D63cf7A8DA1c1cb804A38d51400';
 const TREASURY_ADDRESS = '0xb1556F7Abb3e5D63cf7A8DA1c1cb804A38d51400';
 const MAX_SUPPLY = 5000; // The maximum number of NFTs that can be minted. CANNOT BE UPDATED!
-const MINT_PRICE = 0.001; // Minting price per NFT. Rinkeby = ETH, Polygon = MATIC. CANNOT BE UPDATED!
+const MINT_PRICE = 1; // Minting price per NFT. Rinkeby = ETH, Polygon = MATIC. CANNOT BE UPDATED!
 const TOKENS_PER_MINT = 10; // maximum number of NFTs a user can mint in a single transaction. CANNOT BE UPDATED!
 
 // REQUIRED CONTRACT DETAILS THAT CAN BE UPDATED LATER.
@@ -66,7 +66,7 @@ const PUBLIC_MINT_START_DATE = "2022-04-20T12:00:00+00:00"; // This is required.
 const PRESALE_MINT_START_DATE = "2022-04-02T12:00:00+00:00"; // Optional. Eg: 2022-02-08T11:30:48+00:00
 const ROYALTY_SHARE = 1000; // Percentage of the token price that goes to the royalty address. 100 bps = 1%
 const ROYALTY_ADDRESS = "0xb1556F7Abb3e5D63cf7A8DA1c1cb804A38d51400"; // Address that will receive the royalty
-const BASE_URI = null; // only update if you want to manually set the base uri
+const BASE_URI = "ipfs://bafybeiban7m6mgvxmaeruryvhfy6xhias2mynvabiqzm2dchqa4jkzimqe/"; // only update if you want to manually set the base uri
 const PREREVEAL_TOKEN_URI = null; // only update if you want to manually set the prereveal token uri
 const PRESALE_WHITELISTED_ADDRESSES = ['0xb1556F7Abb3e5D63cf7A8DA1c1cb804A38d51400']; // only update if you want to manually set the whitelisted addresses
 
