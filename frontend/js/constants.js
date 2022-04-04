0dbd089a1fb22686f723d6432930252dbccf6b4f
Quang Do <d.lienquang@gmail.com>
2022-04-03 17:19:42 -0700

    commit

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,5 +1,5 @@
-const contractAddress = "YOUR CONTRACT ADDRESS"; //Replace with your own contract address
-const chain = 'rinkeby'; // rinkeby or polygon
+const contractAddress = "0x0AEFAD36580d6449f5346F9829beadf5d86Db047"; //Replace with your own contract address
+const chain = 'polygon'; // rinkeby or polygon
 
 const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
 const welcome_h2 = "Connect to MetaMask to Get Started";
